d67b8352b380e497a791e36ef72013b3aaa4131f
930211680@qq.com <930211680@qq.com>
2018-05-17 17:38:11 +0800

    移除无效的类

--- poi/src/main/java/com/utils/poi/update_2/data/DataTransfer.java
+++
@@ -1,5 +0,0 @@
-package com.utils.poi.update_2.data;
-
-public interface DataTransfer {
-    String transfer(Object data);
-}
--- poi/src/main/java/com/utils/poi/update_2/data/DataTransferString.java
+++
@@ -1,9 +0,0 @@
-package com.utils.poi.update_2.data;
-
-public class DataTransferString implements DataTransfer {
-
-    @Override
-    public String transfer(Object data) {
-        return data.toString();
-    }
-}
