e49bad98fe34ca7e5c2bee839594935de227fe32
930211680@qq.com <930211680@qq.com>
2018-05-21 18:25:27 +0800

    测试类修改

--- poi/src/main/java/com/utils/poi/update_2/Test.java
+++ poi/src/main/java/com/utils/poi/update_2/Test.java
@@ -1,9 +1,9 @@
 package com.utils.poi.update_2;
 
-import com.utils.poi.bean.Book;
 import com.utils.poi.bean.Student;
 import com.utils.poi.update_2.bean.CellEntity;
 import com.utils.poi.update_2.bean.SheetEntity;
+import com.utils.poi.update_2.dataConversion.DataConversion;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
@@ -17,22 +17,17 @@ public class Test {
         // 测试学生
         ExportExcel<Student> ex = new ExportExcel<Student>();
         // 测试图书
-        ExportExcel<Book> ex2 = new ExportExcel<Book>();
 
-        List<Book> bookList =  getBookList();;
         List<Student> studentList = getStudentList();
 
         try {
 
 
             OutputStream out = new FileOutputStream("E://a.xls");
-            OutputStream out2 = new FileOutputStream("E://b.xls");
 
             ex.exportExcel(getStudentSheetEntity("学生",studentList), out);
-            ex2.exportExcel(getBookSheetEntity("书本",bookList), out2);
 
             out.close();
-            out2.close();
             System.out.println("excel导出成功！");
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,25 +40,10 @@ public class Test {
         cellEntitys.add(new CellEntity("学号","id"));
         cellEntitys.add(new CellEntity("姓名","name"));
         cellEntitys.add(new CellEntity("年龄","age"));
-        cellEntitys.add(new CellEntity("性别","sex"));
+        cellEntitys.add(new CellEntity("性别","sex", new SexDataConversion()));
         cellEntitys.add(new CellEntity("出生日期","birthday"));
 
         SheetEntity entity = new SheetEntity(sheetName,cellEntitys , studentList);
-
-        return entity;
-    }
-
-    private static SheetEntity getBookSheetEntity(String sheetName,List<Book> bookList){
-
-        List<CellEntity> cellEntitys = new ArrayList<CellEntity>();
-        cellEntitys.add(new CellEntity("图书编号","bookId"));
-        cellEntitys.add(new CellEntity("图书名称","name"));
-        cellEntitys.add(new CellEntity("图书作者","author"));
-        cellEntitys.add(new CellEntity("图书价格","price"));
-        cellEntitys.add(new CellEntity("图书ISBN","isbn"));
-        cellEntitys.add(new CellEntity("图书出版社","pubName"));
-
-        SheetEntity entity = new SheetEntity(sheetName, cellEntitys, bookList);
 
         return entity;
     }
@@ -76,15 +56,20 @@ public class Test {
         return datas;
     }
 
-    private static List<Book> getBookList() {
-        List<Book> datas = new ArrayList<>();
-        datas.add(new Book(1, "jsp", "leno", 300.33f, "1234567", "清华出版社"));
-        datas.add(new Book(2, "java编程思想", "brucl", 300.33f, "1234567", "阳光出版社"));
-        datas.add(new Book(3, "DOM艺术", "lenotang", 300.33f, "1234567", "清华出版社"));
-        datas.add(new Book(4, "c++经典", "leno", 400.33f, "1234567", "清华出版社"));
-        datas.add(new Book(5, "c#入门", "leno", 300.33f, "1234567", "汤春秀出版社"));
-        return datas;
+}
+
+class SexDataConversion implements DataConversion{
+
+    @Override
+    public String transferData(Object data) {
+        if (data instanceof Boolean) {
+            boolean bValue = (Boolean) data;
+            String textValue = "男";
+            if (!bValue) {
+                textValue = "女";
+            }
+            return textValue;
+        }
+        return null;
     }
-
-
 }
