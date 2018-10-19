b278e1817e6b6a5a3fe361132307ca9af15e31a1
huangzd <930211680@qq.com>
2018-10-19 08:28:15 +0800

    灰度控制项目初始化

---
+++ gray_control/src/main/java/com/utils/grayControl/GrayControlApplication.java
@@ -0,0 +1,12 @@
+package com.utils.grayControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrayControlApplication.class, args);
+    }
+}
