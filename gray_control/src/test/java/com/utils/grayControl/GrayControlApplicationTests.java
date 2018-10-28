b71c600ffd3a620e05066adfb9a97fe49b339850
huangzd <930211680@qq.com>
2018-10-28 18:03:58 +0800

    灰度控制增加测试代码

--- gray_control/src/test/java/com/utils/grayControl/GrayControlApplicationTests.java
+++ gray_control/src/test/java/com/utils/grayControl/GrayControlApplicationTests.java
@@ -2,6 +2,7 @@ package com.utils.grayControl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GrayControlApplicationTests {
 
+    @Autowired
+    private GrayControlUtils grayControlUtils;
+
     @Test
     public void contextLoads() {
+        boolean gray = grayControlUtils.isControl("gray1", 100232L);
+        System.out.println("是否拦截:"+gray);
+
+
+        gray = grayControlUtils.isControl("gray2", 100232L);
+        System.out.println("是否拦截:"+gray);
     }
 
 }
