3a67d65c284812eb19ddc8a8e2e9b9d6d9d9d6df
huangzd <930211680@qq.com>
2018-11-03 22:54:31 +0800

    状态机工具项目初始化

---
+++ state_machine/src/main/java/com/utils/stateMachine/StateMachineApplication.java
@@ -0,0 +1,12 @@
+package com.utils.stateMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateMachineApplication.class, args);
+    }
+}
