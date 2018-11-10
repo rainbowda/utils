68fe971aa0d3d8eb2ae9f8f724249e4c5a38e5f2
huangzd <930211680@qq.com>
2018-11-10 12:44:02 +0800

    抽象状态机修改

--- state_machine/src/main/java/com/utils/stateMachine/AbstractStateMachine.java
+++ state_machine/src/main/java/com/utils/stateMachine/AbstractStateMachine.java
@@ -1,7 +1,48 @@
 package com.utils.stateMachine;
 
+import com.utils.stateMachine.enums.StateEnum;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 抽象状态机
  */
 public abstract class AbstractStateMachine {
+
+    private Map<StateEnum,List<StateEnum>> stateMachine;
+
+    public void register(){
+        stateMachine = new HashMap<>();
+        buildStateMachine();
+
+    }
+
+    /**
+     * 添加状态
+     * @param currentState
+     * @param nextState
+     */
+    protected void addState(StateEnum currentState, StateEnum nextState){
+        if (currentState == null || nextState == null){
+            throw new NullPointerException();
+        }
+
+        if (stateMachine.get(currentState) == null){
+            stateMachine.put(currentState, new ArrayList<>());
+        }
+        stateMachine.get(currentState).add(nextState);
+    }
+
+
+
+
+    /**
+     * 交给子类实现
+     */
+    public abstract void buildStateMachine();
+
+
 }
