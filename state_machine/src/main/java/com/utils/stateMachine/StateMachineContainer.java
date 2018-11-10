e49dca807b8f96c36a34e241e4d0262ce545c486
huangzd <930211680@qq.com>
2018-11-10 13:19:53 +0800

    状态机容器修改

--- state_machine/src/main/java/com/utils/stateMachine/StateMachineContainer.java
+++ state_machine/src/main/java/com/utils/stateMachine/StateMachineContainer.java
@@ -1,7 +1,49 @@
 package com.utils.stateMachine;
 
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 存放状态机实现的容器
  */
+@Component
 public class StateMachineContainer {
+
+    private final static Map<String, AbstractStateMachine> STATE_MACHINE_MAP = new HashMap<>();
+
+    /**
+     * 往容器中注册状态机
+     * @param stateMachineKey
+     * @param stateMachine
+     */
+    public void registerStateMachine(String stateMachineKey, AbstractStateMachine stateMachine){
+        if (StringUtils.isBlank(stateMachineKey) || stateMachine == null){
+            throw new NullPointerException();
+        }
+
+        //是否已有老状态机
+        AbstractStateMachine oldStateMachine = STATE_MACHINE_MAP.get(stateMachineKey);
+        if (oldStateMachine != null){
+            System.out.println("覆盖"+stateMachineKey+"的状态机");
+        }
+
+        STATE_MACHINE_MAP.put(stateMachineKey, stateMachine);
+    }
+
+    /**
+     * 根据stateMachineKey获取状态机
+     * @param stateMachineKey
+     * @return
+     */
+    public AbstractStateMachine getStateMachine(String stateMachineKey){
+        if (StringUtils.isBlank(stateMachineKey)){
+            return null;
+        }
+        return STATE_MACHINE_MAP.get(stateMachineKey);
+    }
+
 }
