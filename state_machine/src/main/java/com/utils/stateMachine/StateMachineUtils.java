3c25486d0f72f09f6a48a1c760cd3159f50f2ed4
huangzd <930211680@qq.com>
2018-11-11 11:54:56 +0800

    工具类增加是否能扭转状态方法

--- state_machine/src/main/java/com/utils/stateMachine/StateMachineUtils.java
+++ state_machine/src/main/java/com/utils/stateMachine/StateMachineUtils.java
@@ -1,9 +1,36 @@
 package com.utils.stateMachine;
 
+import com.utils.stateMachine.enums.StateEnum;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * 状态机工具
  */
+@Component
 public class StateMachineUtils {
 
+    @Autowired
+    private StateMachineContainer stateMachineContainer;
 
+    /**
+     * 判断当前的状态是否能扭转到传入的下一个状态
+     * @param stateMachineKey
+     * @param currentState
+     * @param nextState
+     * @return
+     */
+    public boolean canOperate(String stateMachineKey, StateEnum currentState, StateEnum nextState){
+        if (StringUtils.isBlank(stateMachineKey) || currentState == null || nextState == null){
+            throw new NullPointerException();
+        }
+
+        AbstractStateMachine stateMachine = stateMachineContainer.getStateMachine(stateMachineKey);
+        if (stateMachine == null){
+            throw new IllegalArgumentException("找不到对应的状态机,stateMachineKey:"+stateMachineKey);
+        }
+
+        return stateMachine.canOperate(currentState, nextState);
+    }
 }
