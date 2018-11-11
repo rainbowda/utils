b2f7ba82f4d49fe92d648252efb00d44801aac99
huangzd <930211680@qq.com>
2018-11-11 11:57:24 +0800

    测试方法

--- state_machine/src/test/java/com/utils/stateMachine/StateMachineApplicationTests.java
+++ state_machine/src/test/java/com/utils/stateMachine/StateMachineApplicationTests.java
@@ -1,7 +1,9 @@
 package com.utils.stateMachine;
 
+import com.utils.stateMachine.enums.StateEnum;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class StateMachineApplicationTests {
 
+    @Autowired
+    private StateMachineUtils stateMachineUtils;
+
     @Test
     public void contextLoads() {
+
+        boolean flag = stateMachineUtils.canOperate("thread", StateEnum.THREAD_NEW, StateEnum.THREAD_TERMINATED);
+        System.out.println(flag);
     }
 
 }
